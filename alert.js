7a1368c7e17085d51b1b8fc59d613f6362070fd5
Alex <zaxco@ya.ru>
2020-12-16 10:24:51 +0300

    first commit

---
+++ alert.js
@@ -0,0 +1,32 @@
+"use strict";
+//alert("Its Ok");
+let a=2;
+console.log(a);
+let arr=['ff','ffg'];
+console.log(arr[1]);
+/*let answer = confirm("Are you here?");
+console.log(answer);
+
+if (answer)
+ {
+     let   ans = +prompt("Сколько тебе лет?", '');
+     console.log(ans);
+     if (ans<18) {
+         alert("маловато");
+     } else {
+         alert("нормально");
+     }
+     
+} else {
+     let ans = +prompt("Через сколько дней вернешься?", '');
+     if (ans<10) {
+         alert("хорошо");
+     } else {
+         alert("долго");
+     }
+     console.log(ans);
+}*/
+let isCheked =true,
+    isClose = true;
+console.log(isCheked && isClose);
+console.log("123");
